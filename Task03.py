790a6eb4d11523b781d65edb712815636f4b6047
Muhammad Abubakar <169022552+ABUBAKAR755@users.noreply.github.com>
2025-03-09 01:28:49 +0500

    Add files via upload

---
+++ Task03.py
@@ -0,0 +1,6 @@
+# Simple Python script example for local use
+def greet(name):
+    print(f"Hello, {name}! Welcome to the Lab Task.")
+
+if __name__ == "__main__":
+    greet("Muhammad Abubakar Khan")
